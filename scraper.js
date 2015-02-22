fb1f5b8dd965e02a3a4bd622f1fff6545b399619
blasian <mikespearman.e@gmail.com>
2015-02-21 23:58:46 -0800

    demo app

--- scraper.js
+++ scraper.js
@@ -1,30 +1,37 @@
 var request = require('request')
 	cheerio = require('cheerio')
-	urls = []
-	courses = []
+	HashMap = require('hashmap').HashMap
+
+var	courses = []
+	urls = new HashMap()
 	// All classes
 	//root = '/cs/main?pname=subjarea&tname=subjareas&req=0'
 	// Only CPSC classes
-	root = '/cs/main?pname=subjarea&tname=subjareas&req=1&dept=CPSC'
+	//root = '/cs/main?pname=subjarea&tname=subjareas&req=1&dept=CPSC'
+	// Only 'THTR' classes
+	root = '/cs/main?pname=subjarea&tname=subjareas&req=1&dept=THTR'
 	base = 'https://courses.students.ubc.ca';
 
 scrape();
 
 function scrape() {
-	urls.push(root);
+	urls.set(200, new Array());
+	urls.get(200).push(root);
+	urls.set(404, new Array());
 	crawl();
 }
 
 function crawl() {
-	console.log(urls.length);
-	if (urls.length == 0) {
+	console.log(urls.get()length);
+	if (urls.get(200).length == 0) {
 		console.log(courses);
 		console.log(courses.length);
 		return;
 	}
-	var link = urls.pop();
+	var link = urls.get(200).pop();
+
 	request(base + link, function(err, resp, body){
-		if (!err && resp.statusCode == 200) {
+		if (!err) {
 			var $ = cheerio.load(body);
 			// if url contains 'req=5' then url is section link
 			if (link.indexOf('req=5') != -1) {
@@ -33,21 +40,19 @@ function crawl() {
 			// if url contains 'req=3' then url is course link
 			else if (link.indexOf('req=3') != -1) {
 				$('tr td a').each(function() {
-					urls.push($(this).attr('href'));
+					urls.get(resp.statusCode).push($(this).attr('href'));
 				});
 			}
 			// if url contains 'req=0' or 'req=0' then url is dept link or main page
 			else if ((link.indexOf('req=0') != -1) || (link.indexOf('req=1') != -1))  {
 				$('#mainTable a').each(function() {
-					urls.push($(this).attr('href'));
+					urls.get(resp.statusCode).push($(this).attr('href'));
 				});
 			}
 			else
 				console.log(base + link);
 			crawl();
-		} else {
-			console.log(resp.statusCode + " occured at " + base + link);
-		};
+		}
 	});
 };
 
