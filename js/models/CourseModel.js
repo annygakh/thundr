38309104720b8b0c90c87e5c83b858666949d6e9
yanlum <kam.naomi@gmail.com>
2015-03-01 23:48:52 -0800

    added default fields to CourseModel and SubSectionModel

--- js/models/CourseModel.js
+++ js/models/CourseModel.js
@@ -2,6 +2,18 @@ var app = app || {};
 
  app.CourseModel = Parse.Object.extend({
  	className: 'TestObject',
+    defaults: {
+		department: "",
+		code: "",
+		credit: "",
+		description: "",
+		pre-reqs: [],
+		co-reqs: [],
+		post-reqs: [],
+		labs: [],
+		tutorials: [],
+		sections = [];
+	},
 
 
 	initialize: function(){
