0c450c4c9914af5d5f53c8ebbe70c741eb6cc0e5
Anny Gakhokidze <annygakhokidze@gmail.com>
2015-03-28 15:02:32 -0700

    fixed typos

--- js/views/SubsectionView.js
+++ js/views/SubsectionView.js
@@ -7,28 +7,28 @@ app.SubsectionView = Backbone.View.extend({
 	tagName: 'div',
 	id: function(){
 		return this.model.get('section_id') + '-' + this.model.get('subsection_id'); //html id will be something like CPSC304-L1A
-	}
+	},
 	// template: TODO
 	events: {
 
 	},
 	initialize: function(){
 
-	}
+	},
 	render: function(){
 		
 		
-	}
+	},
 
 	render_lab: function(){
 
-	}
+	},
 	render_tutorial: function(){
 
-	}
+	},
 	render_course_section: function(){
 
-	}
+	},
 
 
 });
